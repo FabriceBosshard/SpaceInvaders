423979362877ebb84225dc12cdbc1cab7d44ac2c
FabriceBosshard <f.bosshard@bosshard.com>
2017-02-15 12:58:20 +0100

    +changes

--- SpaceInvaders/Laser.cs
+++ SpaceInvaders/Laser.cs
@@ -34,5 +34,6 @@ namespace SpaceInvaders
             };
             return bomb;
         }
+        
     }
 }
--- SpaceInvaders/Player.cs
+++ SpaceInvaders/Player.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.CodeDom;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Diagnostics;
 using System.Linq;
